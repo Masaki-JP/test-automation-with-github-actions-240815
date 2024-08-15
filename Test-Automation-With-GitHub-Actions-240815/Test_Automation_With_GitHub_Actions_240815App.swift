7001d12208adeae7c5e23b7b473c1e0ea9a1e2b0
Masaki-JP <masakidayo0508@gmail.com>
2024-08-15 15:16:26 +0900

    Initial Commit

---
+++ Test-Automation-With-GitHub-Actions-240815/Test_Automation_With_GitHub_Actions_240815App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct Test_Automation_With_GitHub_Actions_240815App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
